dfffc8ee498aea7a5840a805fe27acf0c8fd4c66
Deborah <deborah.dw.50@gmail.com>
2020-06-11 18:38:05 -0400

    Answered Task 2

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(name, cost, category){
-    /* Code here */
+function createMenuItem(name, price, category){
+    return {name,price, category};
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+console.log(createMenuItem('Tacos', 8, 'Lunch'));
+console.log(createMenuItem('Poke Bowl', 15, 'Lunch'));
+console.log(createMenuItem('Milkshake', 8, 'Drinks'));
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -24,7 +27,17 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
-
+burger.discount = function(customer){
+  if (customer === 'teacher'){
+  return burger.price - (burger.price * .25)
+  }else if (customer === 'student'){
+    return burger.price - (burger.price * .25)
+    }else (customer === 'public');{
+      return burger.price - (burger.price * .10) 
+    }
+}
+console.log(burger.discount('teacher'));
+console.log(burger.discount('public'));
 
 ///////////////Reviews (MVP)///////////////////
 
